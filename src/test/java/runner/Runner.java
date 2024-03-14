840c5b589a28188dd336255c5ac797ff28982596
i-am-shipwrecked <zapasnoy2k17@mail.ru>
2024-03-14 15:57:04 +0200

    14.03.24 / serenity added to the project

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -2,15 +2,22 @@ package runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.picocontainer.PicoFactory;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         publish = true,
         stepNotifications = true,
         tags = "@Run",
         features = "src/test/java/features",
-        glue = "steps"
+        glue = "steps",
+        plugin =    {
+                "pretty", "html:target/serenity-reports/serenity-html-report",
+                "json:target/serenity-reports/SerenityTestReport.json",
+                "rerun:target/serenity-reports/rerun.txt"},
+        objectFactory = PicoFactory.class
 )
 public class Runner {
 }
